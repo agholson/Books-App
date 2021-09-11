71af95e57bd85e341b4bfeac3a7adf5704df7db6
agholson <87455899+agholson@users.noreply.github.com>
2021-09-11 12:15:26 -0400

    Initial Commit

---
+++ Books-App-Lesson15-Challenge/Books_App_Lesson15_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Books_App_Lesson15_ChallengeApp.swift
+//  Books-App-Lesson15-Challenge
+//
+//  Created by Shepherd on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Books_App_Lesson15_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
